3a6bc5130b0948333c155c536658b60dc5dec88e
qabuilder <qabuilder@nvidia.com>
2019-07-05 00:56:24 +0530

    Predict velocity and position in x and y dimension
    Add the matrices, and initialize values which will allow prediction of
    velocity in x and y direction

--- kalman_nd.py
+++ kalman_nd.py
@@ -5,15 +5,39 @@ class KalmanNDimensions(object):
     """Class implements a kalman filter for measurements and predictions in "n" dimensions
     """
     def __init__(self):
+        '''
         # Movement and velocity in 1 dimention, lets say x
         self.measurements = np.array([1, 2, 3], np.float) #measurement inputs recieved, lets say x co-ordinates
-        self.x = np.array([[0.], [0.]], np.double) # initial state (location and velocity)
-        self.P = np.array([[1000., 1.], [0., 1000.]], np.double) # initial uncertainty(covariance)
-        self.u = np.array([[0.], [0.]], np.double) # external motion
-        self.F = np.array([[1., 1.], [0, 1.]], np.double) # next state function
-        self.H = np.array([[1., 0.]], np.double) # measurement function
-        self.R = np.array([[1.]], np.double) # measurement uncertainty
-        self.I = np.array([[1., 0.], [0., 1.]], np.double) # identity matrix
+        self.x = np.array([[0.], [0.]], np.float) # initial state (location and velocity)
+        self.P = np.array([[1000., 1.], [0., 1000.]], np.float) # initial uncertainty(covariance)
+        self.u = np.array([[0.], [0.]], np.float) # external motion
+        self.F = np.array([[1., 1.], [0, 1.]], np.float) # next state function
+        self.H = np.array([[1., 0.]], np.float) # measurement function
+        self.R = np.array([[1.]], np.float) # measurement uncertainty
+        self.I = np.array([[1., 0.], [0., 1.]], np.float) # identity matrix
+        '''
+
+        #N dimension state, x,y, and velocity in x direction and y direction
+        #frequency of measurement update
+        self.dt = 0.1
+        self.measurements = np.array([[5., 10.], [6., 8.], [7., 6.], [8., 4.], [9., 2.], [10., 0.]], np.float)
+        #name should be xy,or just "state" but keeping it as x to be compatible to previous versions
+        #this represents x,y/veocity_x, velocity_y
+        self.x = np.array( [[4.],[12.], [0.], [0.]], np.float) # initial state (location and velocity)
+        self.F = np.array([ [1., 0., self.dt, 0.     ],\
+                            [0., 1., 0.     , self.dt],\
+                            [0., 0., 1.     , 0.      ],\
+                            [0., 0., 0.     , 1.      ]], np.float) # next state function
+        self.P = np.array([ [0., 0., 0.     , 0.     ],\
+                            [0., 0., 0.     , 0.     ],\
+                            [0., 0., 1000.  , 0.     ],\
+                            [0., 0., 0.     , 1000.  ]], np.float) # co-variance matrix. Start with a large un certainty
+
+        self.u = np.array([[0.], [0.], [0.], [0.]], np.float) # external motion
+        self.H = np.array([[1., 0., 0., 0.],\
+                           [0., 1., 0., 0.]], np.float) # measurement function
+        self.R = np.array([[0.1, 0.0], [0.0, 0.1]], np.float) # measurement uncertainty
+        self.I = np.identity(4, np.float) # identity matrix
 
     def predict(self):
         """Prediction of next state
@@ -24,8 +48,8 @@ class KalmanNDimensions(object):
     def measure(self, measurement):
         """Get the measurement update
         """
-        measurement = np.array(measurement)
-        y = measurement - np.dot(self.H, self.x) #Difference between the measured value, and previously predicted one
+        measurement = np.array([measurement])
+        y = np.transpose(measurement) - np.dot(self.H, self.x) #Difference between the measured value, and previously predicted one
         s = np.dot(np.dot(self.H, self.P), np.transpose(self.H)) + self.R  #Measurement uncertainty + measurement noise
         k = np.dot(np.dot(self.P, np.transpose(self.H)),np.linalg.inv(s)) #kalman gain
         self.x = self.x + np.dot(k, y) #new measurement = old measurement + kalman gain * difference
